15ecc76173dbc10a16911ce0af5738d7f7f000cf
ahmetbugraerturk <bugra20042010@gmail.com>
2023-08-08 15:43:34 +0300

    login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:archery/screens/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -9,7 +10,7 @@ void firebaseInit() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-}
+} //Firebase asenkron başlatıllıyor
 
 void main() async{
   runApp(const MyApp());
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, //Debug yazısını kaldırıyor
       home: MyHomePage(),
     );
   }
@@ -37,9 +39,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Arcoach",
       theme: ThemeData(
-        primaryColor: Colors.blue
       ),
       localizationsDelegates: [
         AppLocalizations.delegate,
@@ -50,8 +52,150 @@ class _MyHomePageState extends State<MyHomePage> {
       supportedLocales: [
         Locale("en"),
         Locale("tr")
-      ],
-      home: login(),
+      ], //Locale kodları dil ayarlıyor
+      home: loginAndRegister(),
     );
   }
 }
+
+class loginAndRegister extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => loginAndRegisterPage();
+}
+
+class loginAndRegisterPage extends State<loginAndRegister>{
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  bool login = true; //Splash ekranında login-register kısımlarının durumunu ayarlıyor
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF607262),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  login = true;
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease,
+                height: login ? MediaQuery.of(context).size.height * 0.6 : MediaQuery.of(context).size.height * 0.15,
+                child: CustomPaint(
+                  painter: CurvePainter(login),
+                  child: Container(
+                    padding: EdgeInsets.only(bottom: login ? 0 : 55),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                          child: login
+                              ? Column(
+                                  children: [
+                                    Login(),
+                                    SizedBox(
+                                      height: 14,
+                                    ),
+                                    GestureDetector(
+                                      onTap: (){
+                                        setState(() {
+                                          login = !login;
+                                        });
+                                      },
+                                      child: Center(
+                                        child: Text("Don't have an account?",
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Color(0xFF1C1C1C),
+                                            height: 1,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              : LoginOption(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            GestureDetector(
+              onTap: () {
+                /*setState(() {
+                  login = false;
+                });*/
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease,
+                height: login ? MediaQuery.of(context).size.height * 0.4 : MediaQuery.of(context).size.height * 0.6,
+                child: Container(
+                    color: Colors.transparent,
+                    padding: EdgeInsets.only(top: login ? 55 : 0),
+                    child: Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                          /*child: !login
+                              ? SignUp()
+                              : SignUpOption(),*/
+                        ),
+                      ),
+                    )
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+
+  bool outterCurve;
+
+  CurvePainter(this.outterCurve);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Color(0xFFBCCFBB);
+    paint.style = PaintingStyle.fill;
+
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(size.width * 0.5, outterCurve ? size.height + 110 : size.height - 110, size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+} //Süs
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,57 +1,203 @@
+import 'package:archery/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class login extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => loginPage();
-}
-
-class loginPage extends State<login>{
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
-  int _counter = 0;
-
-  void _incrementCounter() {
-    //Navigator.push(context, MaterialPageRoute(builder: (context)=>login()));
-    setState(() {
-      _counter++;
-    });
-  }
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(AppLocalizations.of(context).appName),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              Localizations.localeOf(context).languageCode,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Welcome to",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF1C1C1C),
+            height: 2,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        Text(
+          "ARCOACH",
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF1C1C1C),
+            letterSpacing: 2,
+            height: 1,
+          ),
+        ),
+        Text(
+          "Please login to continue",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF1C1C1C),
+            height: 1,
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        TextField(
+          style: TextStyle(
+            fontSize: 16,
+            color: Color(0xFFBCCFBB),
+            fontWeight: FontWeight.bold,
+          ),
+          decoration: InputDecoration(
+            hintText: 'Email',
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: Color(0xFFBCCFBB),
+              fontWeight: FontWeight.bold,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(
+                width: 0,
+                style: BorderStyle.none,
+              ),
+            ),
+            filled: true,
+            fillColor: Color(0xFF607262),
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+          ),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        TextField(
+          style: TextStyle(
+            fontSize: 16,
+            color: Color(0xFFBCCFBB),
+            fontWeight: FontWeight.bold,
+          ),
+          decoration: InputDecoration(
+            hintText: 'Password',
+            hintStyle: TextStyle(
+              fontSize: 16,
+              color: Color(0xFFBCCFBB),
+              fontWeight: FontWeight.bold,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide: BorderSide(
+                width: 0,
+                style: BorderStyle.none,
+              ),
+            ),
+            filled: true,
+            fillColor: Color(0xFF607262),
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+          ),
+        ),
+        SizedBox(
+          height: 24,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 40,
+            decoration: BoxDecoration(
+              color: Color(0xFF1C1C1C),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF1C1C1C).withOpacity(0.2),
+                  spreadRadius: 3,
+                  blurRadius: 4,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFFBCCFBB),
+                ),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 25,
+        ),
+        Center(
+          child: Text(
+            "Forgot Password?",
+            style: TextStyle(
+              fontSize: 14,
+              color: Color(0xFF1C1C1C),
+              height: 1,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: FloatingActionButton(
+                  backgroundColor: Color(0xFF607262),
+                  onPressed: (){},
+                  child: SizedBox(
+                    height: 35,
+                      width: 35,
+                      child: Image.asset("lib/assets/googleLogo.png"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Center(
+                child: FloatingActionButton(
+                  backgroundColor: Color(0xFF607262),
+                  onPressed: (){},
+                  child: SizedBox(
+                    height: 35,
+                    width: 35,
+                    child: Image.asset("lib/assets/appleLogo.png"),
+                  ),                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Center(
+                child: FloatingActionButton(
+                  backgroundColor: Color(0xFF607262),
+                  onPressed: (){},
+                  child: SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: Image.asset("lib/assets/facebookLogo.png"),
+                  ),                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class LoginOption extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[],
     );
   }
 }
